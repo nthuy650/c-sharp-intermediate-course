69ce91841c3a30d433b1b517f1816134cf72154d
nthuy650 <nthuy.wd@gmail.com>
2022-11-13 03:50:27 +0100

    using method overloadding

---
+++ c-sharp-intermediate/Point.cs
@@ -0,0 +1,28 @@
+﻿namespace c_sharp_intermediate;
+
+public class Point
+{
+    public int X;
+
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    //move method
+    public void Move(int x, int y)
+    {
+        this.X = x;
+        this.Y = y;
+    }
+
+    //move method overloadding
+    public void Move(Point point)
+    {
+        this.X = point.X;
+        this.Y = point.Y;
+    }
+}
--- c-sharp-intermediate/Program.cs
+++ c-sharp-intermediate/Program.cs
@@ -4,26 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        //create a Customer instance
-        var customer1 = new Customer();
-        // default constructor will assign 0 to Id and null to Name
-        Console.Write(customer1.Id); //0
-        Console.WriteLine(customer1.Name); //null
+        var myPoint = new Point(1, 2);
+        myPoint.Move(10, 10);
+        Console.WriteLine("My new point is in ({0}, {1})", myPoint.X, myPoint.Y);
 
-        // using the first constructor
-        var customer2 = new Customer(1);
-        Console.Write(customer2.Id); //1
-        Console.WriteLine(customer2.Name); //null
-
-        // using the second constructor
-        var customer3 = new Customer(2, "John");
-        Console.Write(customer3.Id); //2
-        Console.WriteLine(customer3.Name); //John
-
-        var order = new Order();
-        // this will throw error: Object reference is not set to an instance of an object
-        // if customer1 didn't initialize Order in its constructor
-        // if we comment the line 14 in the Customer.cs
-        customer1.Orders.Add(order);
+        myPoint.Move(new Point(20, 20));
+        Console.WriteLine("My new point is in ({0}, {1})", myPoint.X, myPoint.Y);
     }
 }
