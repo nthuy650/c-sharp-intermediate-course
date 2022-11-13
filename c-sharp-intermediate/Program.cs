9d465f8234bfe0f2057704c7cc6c9b292616e926
nthuy650 <nthuy.wd@gmail.com>
2022-11-13 03:54:43 +0100

    add global error handling to prevent application crashed

--- c-sharp-intermediate/Program.cs
+++ c-sharp-intermediate/Program.cs
@@ -4,11 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        var myPoint = new Point(1, 2);
-        myPoint.Move(null);
-        Console.WriteLine("My new point is in ({0}, {1})", myPoint.X, myPoint.Y);
+        try
+        {
+            var myPoint = new Point(1, 2);
+            myPoint.Move(null);
+            Console.WriteLine("My new point is in ({0}, {1})", myPoint.X, myPoint.Y);
 
-        myPoint.Move(new Point(20, 20));
-        Console.WriteLine("My new point is in ({0}, {1})", myPoint.X, myPoint.Y);
+            myPoint.Move(new Point(20, 20));
+            Console.WriteLine("My new point is in ({0}, {1})", myPoint.X, myPoint.Y);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("an unexpected error occured!");
+        }
     }
 }
